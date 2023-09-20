b01b20478548766f1e0992935449a01325a09bcf
nabilroyyan <nabil.royyan1203@gmail.com>
2023-09-20 11:39:07 +0700

    bab 4

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -4,10 +4,22 @@ const router = express.Router();
 const {body, validationResult} = require('express-validator');
 
 const connection = require("../config/db");
-
-router.get('/', function(req, res){
-
-});
+router.get('/', function (req, res){
+     connection.query('select * from mahasiswa order by id_m desc', function(err, rows){
+         if(err){
+             return res.status(500).json({
+                 status: false,
+                 message: 'Server Failed',
+             })
+         }else{
+             return res.status(200).json({
+                 status:true,
+                 message: "Data Mahasiswa",
+                 data: rows
+             })
+         }
+     })
+ });
 
 router.post('/store',[
      //validation
@@ -38,7 +50,37 @@ router.post('/store',[
                })
           }
      })
-})
+});
+
+
+router.get('/(:id)', function(req, res){
+     let id = req.params.id;
+     connection.query(`select * from mahasiswa where id_m = ${id}`, function (err, rows){
+          if(err){
+               return res.status(500).json({
+                    status: false,
+                    massage: 'server eror',
+               })
+          }
+          if(rows.length <= 0){
+               return res.status(404).json({
+                    status: false,
+                    massage: 'not found',
+               })
+          }  
+          else{
+               return res.status(200).json({
+                    status: true,
+                    massage: 'data mahasiswa',
+                    data: rows[0]
+               })
+          }
+     }) 
+});
+
+
+
+
 
 
 module.exports = router;
